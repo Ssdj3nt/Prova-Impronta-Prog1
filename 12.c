7963a6c65d396431f0e0ed2e498a43a3c1032c53
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2022-02-24 13:33:39 +0100

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,3 +1,10 @@
+/*
+	Sviluppare una function C che, data come parametro di input una stringa che
+	rappresenta un testo in italiano, determina e restituisce
+ 	come parametri di output la parola di lunghezza massima contenuta nel testo
+	e la sua lunghezza. Nel testo le parole sono separate da
+ 	un unico spazio. 
+*/
 void traccia12(char *testo, char *parola_max, int *max){
 	*max=0;
 	char *tmp=strtok(testo, " ");
