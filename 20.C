dc5e0920c2d89315cb8a4798dc89eb0914f85af2
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2022-02-28 10:54:58 +0100

    Update 20.C

--- 20.C
+++ 20.C
@@ -17,11 +17,12 @@ struct partecipante{
     unsigned short codice;
 };
 typedef struct partecipante p;
-void traccia20(p Partecipante[]);
+void traccia20(p Partecipante[],int n);
 void Scambio(p *a, p *b);
 void main()
 {
-    p partecipante[100];
+    p partecipante[10];
+    size_t n = sizeof (partecipante)/sizeof (partecipante[0]);
 
     partecipante[0].utente.nome="Francesco";
     partecipante[0].utente.cognome="Porritiello";
@@ -38,36 +39,27 @@ void main()
     partecipante[3].utente.nome="Nicola";
     partecipante[3].utente.cognome="Errichiello";
     partecipante[3].codice=2489;
-    traccia20(partecipante);
+    traccia20(partecipante,n);
 }
-void traccia20(p Partecipante[])
+void traccia20(p Partecipante[],int n)
 {
     int i;
     int ult_scambio;
     int fine_ord;
-    int n=3;
     fine_ord=n-1;
 
     while (fine_ord!=0)
-    {   // Se non ci sono più elementi da scambiare porto ultimo scambio a 0
-        // Darà il suo valore a fine_ord e usciremo dal while
+    {
         ult_scambio=0;
-        // Controllo tutti gli elementi della struct
         for (i=0; i<fine_ord; i++)
-        {   // Se il partecipante di indice I è alfabeticamente maggiore del partecipante di indice I+1
+        {
             if (strcmp(Partecipante[i].utente.cognome, Partecipante[i+1].utente.cognome) > 0)
-            { // Scambio i dati
+            {
                 Scambio(&Partecipante[i], &Partecipante[i+1]);
-                // E porto l'ultimo scambio effettuato a I
                 ult_scambio=i;
             }
-        } // Riporto la I a ultimo scambio, se l'if non si verifica più, nella prossima iterazione
-        // fine_ord avrà il valore 0
+        }
         fine_ord=ult_scambio;
-    }
-    // Stampo infine l'array ordinato
-    for (i=0;i<n;i++) {
-        puts(Partecipante[i].utente.cognome);
     }
 }
 void Scambio(p *a, p *b)
